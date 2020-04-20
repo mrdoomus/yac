bf49fb3b3297a5f15e5424e9a5f2bd28331faccc
mrdoomus <mrdoom.official@gmail.com>
2020-04-20 12:17:17 -0500

    Updates

---
+++ precache-manifest.1bcae587e64046ff9a870819ebeb5578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa6b5c8c03b592f015db241b9a46606",
+    "url": "/yac/index.html"
+  },
+  {
+    "revision": "975d5c594327ea85b7f9",
+    "url": "/yac/static/css/main.a1608811.chunk.css"
+  },
+  {
+    "revision": "a67922ee7ddb073ccfa0",
+    "url": "/yac/static/js/2.71a23e2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yac/static/js/2.71a23e2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975d5c594327ea85b7f9",
+    "url": "/yac/static/js/main.d780c80d.chunk.js"
+  },
+  {
+    "revision": "f78488ceb3cc8da25696",
+    "url": "/yac/static/js/runtime-main.4ab06e4f.js"
+  }
+]);
